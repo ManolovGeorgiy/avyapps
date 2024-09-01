405ea9316e3483db349ada5f591067bead773a29
Georgiy <g.manolov@web.de>
2024-09-01 16:39:17 +0200

    footer

--- src/test/java/e2e/tests/HomeTest.java
+++ src/test/java/e2e/tests/HomeTest.java
@@ -1,5 +1,6 @@
 package e2e.tests;
 
+import com.github.javafaker.Faker;
 import e2e.enums.FooterLinks;
 import e2e.enums.LandingNavBar;
 import e2e.pages.AddDemoForm;
@@ -10,18 +11,20 @@ import org.testng.annotations.Test;
 
 public class HomeTest extends TestBase{
 
+    Faker faker = new Faker();
     HomePage homePage;
     Header header;
     Footer footer;
     AddDemoForm addDemoForm;
+
     @Test
     public void homeTest(){
         String name = "Georg";
         String nameCompany = "Georg";
         String email = "g@gmail.com";
         String phone = "+1234567";
-        String country = "Frace";
-        String message = "Georg,gggggkfkfkfkfkf";
+        String country = "France";
+        String message = faker.lorem().sentence(50);
 
         homePage = new HomePage(app.driver);
         homePage.waitForLoading();
@@ -36,9 +39,8 @@ public class HomeTest extends TestBase{
         homePage = new HomePage(app.driver);
         addDemoForm = new AddDemoForm(app.driver);
         addDemoForm.setPostForm(name,nameCompany,email,phone,country,message);
-
-
-        //homePage.scrollDown();
-        //homePage.clickJoinButton();
+        addDemoForm.clickDemoButton();
+        homePage.scrollDown();
+        homePage.clickJoinButton();
     }
 }

4a11ea240fd1c8c67975f49377342d592c81162f
Georgiy <g.manolov@web.de>
2024-09-01 16:32:08 +0200

    footer

--- src/test/java/e2e/pages/AddDemoForm.java
+++ src/test/java/e2e/pages/AddDemoForm.java
@@ -1,5 +1,7 @@
 package e2e.pages;
 
+import io.qameta.allure.Step;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -12,7 +14,7 @@ public class AddDemoForm extends BasePage{
     WebElement nameCompanyInput;
     @FindBy(xpath = "//*[@placeholder='Email']")
     WebElement emailInput;
-    @FindBy(xpath = "//select[@placeholder='Phone']")
+    @FindBy(xpath = "//*[@placeholder='Phone']")
     WebElement phoneInput;
     @FindBy(xpath = "//*[@class='LandingForm_countryInput__lJl1W']")
     WebElement countryInput;
@@ -24,5 +26,31 @@ public class AddDemoForm extends BasePage{
     WebElement bookADemoButton;
     public AddDemoForm(WebDriver driver) {
         super(driver);
+    }
+
+    @Step("Wait for loading Edit profile page")
+    public void waitForLoading() {
+        try {
+            getWait().forVisibility(nameInput);
+            getWait().forVisibility(nameCompanyInput);
+            getWait().forVisibility(emailInput);
+            getWait().forVisibility(phoneInput);
+            getWait().forVisibility(countryInput);
+            getWait().forVisibility(employeeSizeInput);
+            getWait().forVisibility(messageInput);
+            getWait().forVisibility(bookADemoButton);
+        } catch (StaleElementReferenceException e) {
+        }
+    }
+    @Step("Fill demo form")
+    public void setPostForm(String name, String company, String email, String phone,String country,String message) {
+        nameInput.sendKeys(name);
+        nameCompanyInput.sendKeys(company);
+        emailInput.sendKeys(email);
+        phoneInput.sendKeys(phone);
+        messageInput.sendKeys(message);
+    }
+    public void clickDemoButton(){
+        bookADemoButton.click();
     }
 }
--- src/test/java/e2e/tests/HomeTest.java
+++ src/test/java/e2e/tests/HomeTest.java
@@ -2,6 +2,7 @@ package e2e.tests;
 
 import e2e.enums.FooterLinks;
 import e2e.enums.LandingNavBar;
+import e2e.pages.AddDemoForm;
 import e2e.pages.Footer;
 import e2e.pages.Header;
 import e2e.pages.HomePage;
@@ -12,16 +13,32 @@ public class HomeTest extends TestBase{
     HomePage homePage;
     Header header;
     Footer footer;
-
+    AddDemoForm addDemoForm;
     @Test
     public void homeTest(){
+        String name = "Georg";
+        String nameCompany = "Georg";
+        String email = "g@gmail.com";
+        String phone = "+1234567";
+        String country = "Frace";
+        String message = "Georg,gggggkfkfkfkfkf";
+
         homePage = new HomePage(app.driver);
         homePage.waitForLoading();
         homePage.scrollToFooter();
         footer = new Footer(app.driver);
         footer.clickFooterButton(FooterLinks.ABOUT);
         homePage = new HomePage(app.driver);
-        homePage.scrollDown();
-        homePage.clickJoinButton();
+        homePage.waitForLoading();
+        homePage.scrollToFooter();
+        footer = new Footer(app.driver);
+        footer.clickFooterButton(FooterLinks.DEMO);
+        homePage = new HomePage(app.driver);
+        addDemoForm = new AddDemoForm(app.driver);
+        addDemoForm.setPostForm(name,nameCompany,email,phone,country,message);
+
+
+        //homePage.scrollDown();
+        //homePage.clickJoinButton();
     }
 }

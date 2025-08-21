c976afc75c9e8a50eaf4ab8c16f20b1c2cfa13d2
Georgiy <g.manolov@web.de>
2025-08-21 14:03:57 +0200

    footer

--- src/test/java/e2e/pages/HomePage.java
+++ src/test/java/e2e/pages/HomePage.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.support.FindBy;
 
 public class HomePage extends BasePage {
 
-    @FindBy(xpath = "//*[@class=LandingForm_formButton__dvhs7']")
+    @FindBy(xpath = "//*[@class=LandingJoin_joinButton__GNwZp']")
     WebElement joinButton;
 
     @FindBy(xpath = "//*[@class='LandingNavBar_landingButtonV1__p1Wiy']")
